0c98b59f418f00387b100478a8762f9b0467378c
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-04-25 18:12:18 +0200

    Update SQL

--- es_extended.sql
+++ es_extended.sql
@@ -2,8 +2,8 @@ USE `gta5_gamemode_essential`;
 
 ALTER TABLE `users` 
 ADD COLUMN `skin` LONGTEXT NULL AFTER `money`,
-ADD COLUMN `job` INT NULL DEFAULT -1 AFTER `skin`,
-ADD COLUMN `job_grade` INT NULL DEFAULT -1 AFTER `job`;
+ADD COLUMN `job` INT NULL DEFAULT 0 AFTER `skin`,
+ADD COLUMN `job_grade` INT NULL DEFAULT 0 AFTER `job`;
 
 CREATE TABLE `items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
